9136bb8955c700670ab476123f509b012dd86392
amanueltamer <amanueltamer@gmail.com>
2023-04-02 11:44:32 -0500

    added prevPost for backbutton in thread.

--- src/features/postSlice.js
+++ src/features/postSlice.js
@@ -4,18 +4,23 @@ export const postSlice = createSlice({
   name: "post",
   initialState: {
     selectedPost: null,
+    // prevPostId: null,
+    prevPost: null,
   },
   reducers: {
     selectPost: (state, action) => {
+      state.prevPost = state.selectedPost;
       state.selectedPost = action.payload;
-    }
+    //   state.prevPostId = prevPost?.postId;
+    },
   },
 });
 
-export const {
-  selectPost
-} = postSlice.actions;
+export const { selectPost } = postSlice.actions;
 
 export const selectOpenPost = (state) => state.post.selectedPost;
+
+// export const selectPrevPostId = (state) => state.post.prevPostId;
+export const selectPrevPost = (state) => state.post.prevPost;
 
 export default postSlice.reducer;
