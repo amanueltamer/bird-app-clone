77a40d7e3c2ac0b9c5c924836d3645aa0e387c36
amanueltamer <amanueltamer@gmail.com>
2023-04-02 11:35:50 -0500

    changed comments to posts & added "commentPost"

--- src/components/Thread.jsx
+++ src/components/Thread.jsx
@@ -17,12 +17,12 @@ import {
 } from "@mui/icons-material";
 import { Avatar, Button, CircularProgress, IconButton } from "@mui/material";
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 // import { useSelector } from 'react-redux';
 import { useNavigate, useParams } from "react-router-dom";
 // import { selectOpenPost } from '../features/postSlice';
 import "../css/Thread.css";
-import { selectOpenPost } from "../features/postSlice";
+import { selectOpenPost, selectPrevPost, selectPrevPostId } from "../features/postSlice";
 import { selectUser } from "../features/userSlice";
 import { auth, db } from "../firebase-config";
 // import Post from "./Post";
@@ -30,9 +30,10 @@ import firebase from "firebase/compat/app";
 import Post from "./Post";
 import Comments from "./Comments";
 import FlipMove from "react-flip-move";
+import { selectPost } from "../features/postSlice";
 
 
-export default function Thread() {
+export default function Thread({displayName, username, verified, text, image, avatar}) {
   const [commentMessage, setCommentMessage] = useState("");
   const [commentImage, setCommentImage] = useState("");
   const [error, setError] = useState("");
@@ -41,7 +42,10 @@ export default function Thread() {
   const [loading, setLoading] = useState(false);
   const [comments, setComments] = useState([]);
   const [like, setLike] = useState(false);
-  
+  const [posts, setPosts] = useState([]);
+
+  const dispatch = useDispatch();
+
   const handleLike = () => {
     setLike(!like);
   };
@@ -49,6 +53,48 @@ export default function Thread() {
   const { postId, commentsId } = useParams();
 
   const user = useSelector(selectUser);
+  const selectedPost = useSelector((state) => selectOpenPost(state, postId));
+//   const prevPostId = useSelector(selectPrevPostId)
+const prevPost = useSelector(selectPrevPost);
+
+
+//   const openPost = () => {
+//     dispatch(
+//       selectPost({
+//         displayName,
+//         username,
+//         verified,
+//         text,
+//         image,
+//         avatar,
+//         postId,
+//       })
+//     );
+//     console.log(displayName);
+//   };
+const goBack = () => {
+    if (!selectedPost.commentPost) {
+      // Navigate to home page
+      navigate('/');
+    } else if (prevPost && prevPost.postId !== selectedPost.postId) {
+      // Navigate to previous post
+      navigate(`/thread/${prevPost.postId}`);
+      // Set previous post as selectedPost
+      dispatch(selectPost(prevPost));
+    } else {
+      // Navigate to thread page for the same post
+      navigate(`/thread/${selectedPost.postId}`);
+    }
+  };
+  
+  
+
+  useEffect(() => {
+    if (!selectedPost || selectedPost.postId !== postId) {
+      // Fetch post data for postId and dispatch selectPost action
+      dispatch(selectPost({ postId }));
+    }
+  }, [dispatch, postId, selectedPost]);
 
   const textareaOpener = () => {
     setTextareaOpened(true);
@@ -61,18 +107,22 @@ export default function Thread() {
 
   useEffect(() => {
     setLoading(true);
-    db.collection("comments")
+    // Listen for new posts and add them to the posts array
+    const unsubscribe = db.collection("posts")
+      .where("commentPost", "==", true)
       .where("postId", "==", postId)
       .orderBy("timestamp", "desc")
       .onSnapshot(function (snapshot) {
-        setComments(
-          snapshot.docs.map(function (doc) {
-            return { ...doc.data(), id: doc.id };
-          })
-        );
-        setLoading(false);
+        const newPosts = snapshot.docs.map(function (doc) {
+          setLoading(false);
+          return { ...doc.data(), id: doc.id };
+        });
+        setPosts(newPosts);
       });
-  }, []);
+    // Unsubscribe from the listener when the component unmounts
+    return unsubscribe;
+  }, [postId]);
+  
 
   const sendComment = (e) => {
     e.preventDefault();
@@ -107,7 +157,7 @@ export default function Thread() {
 
     console.log(user);
 
-    db.collection("comments")
+    db.collection("posts")
       .add({
         postId: postId, // the ID of the post that the comment is associated with
         displayName: user.displayName,
@@ -118,9 +168,23 @@ export default function Thread() {
         avatar: user.photoUrl,
         userId: userId, // add user ID field to post
         timestamp: firebase.firestore.FieldValue.serverTimestamp(), // add timestamp field to post
+        commentPost: true,
       })
       .then((docRef) => {
         console.log("Document written with ID: ", docRef.id);
+        const newComment = {
+            id: docRef.id,
+            displayName: user.displayName,
+            username: user?.displayName?.replace(/\s/g, ""),
+            verified: true,
+            text: commentMessage || null,
+            image: commentImage || null,
+            avatar: user.photoUrl,
+            userId: userId,
+            timestamp: new Date().toISOString(),
+            commentPost: true,
+          };
+        setPosts([newComment, ...posts]);
         setCommentMessage("");
         setCommentImage("");
       })
@@ -139,21 +203,23 @@ export default function Thread() {
 
   const navigate = useNavigate();
 
-  const selectedPost = useSelector(selectOpenPost);
-
   console.log(selectedPost);
 
   console.log(postId);
   console.log(comments);
   console.log(commentsId);
-  
+  console.log(posts);
+
 
   return (
     <div className="thread">
       <div className="thread__container">
         <div className="thread__header">
           <IconButton
-            onClick={() => navigate("/")}
+            onClick={() => {
+                goBack()
+                // navigate(-1);
+            }}
             className="thread__backButton"
           >
             <ArrowBack />
@@ -206,10 +272,12 @@ export default function Thread() {
 
         <hr />
 
+        <p className="thread__replyingTo">Replying to <span className="thread__replyingToName">@{selectedPost?.username}</span></p>
+
         <div className="thread__inputContainer">
             <div className="thread__avatar">
           <Avatar src={selectedPost?.avatar} />
-            </div>
+        </div>
 
           <form>
             {textareaOpened ? (
@@ -278,28 +346,30 @@ export default function Thread() {
         </div>
       </div>
 
-      {loading ? (
+      {loading && !posts ? (
   <div className="loading__animation">
     <CircularProgress />
   </div>
 ) : null}
 
-{!loading && comments && comments.length > 0 ? (
+{!loading && posts && posts.length > 0 ? (
   <FlipMove>
-    {comments.map((comments) => (
-      <Comments
-        key={comments.id}
-        displayName={comments.displayName}
-        username={comments.username}
-        verified={comments.verified}
-        text={comments.text}
-        avatar={comments.avatar}
-        image={comments.image}
-        commentsId={comments.id}
+    {posts.map((post) => (
+      <Post
+        key={post.id}
+        displayName={post.displayName}
+        commentPost={post.commentPost}
+        username={post.username}
+        verified={post.verified}
+        text={post.text}
+        avatar={post.avatar}
+        image={post.image}
+        postId={post.id}
       />
     ))}
   </FlipMove>
 ) : null}
+
 
     </div>
   );
