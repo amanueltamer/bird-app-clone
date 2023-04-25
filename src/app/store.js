c6fa2969f43eaa5d2b9d5780670f17ddd9c3660a
amanueltamer <amanueltamer@gmail.com>
2023-04-25 16:01:35 -0500

    middleware added to fix payload.timestamp issues.

--- src/app/store.js
+++ src/app/store.js
@@ -2,9 +2,22 @@ import { configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/userSlice';
 import postReducer from '../features/postSlice';
 
+const middleware = (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+    serializableCheck: {
+      // Ignore these action types
+      ignoredActions: ['your/action/type'],
+      // Ignore these field paths in all actions
+      ignoredActionPaths: ['payload.timestamp', 'post.selectedPost.timestamp', 'post.prevPost.timestamp'],
+      // Ignore these paths in the state
+      ignoredPaths: ['items.dates'],
+    },
+  });
+
 export const store = configureStore({
   reducer: {
     user: userReducer,
     post: postReducer,
   },
+  middleware,
 });
