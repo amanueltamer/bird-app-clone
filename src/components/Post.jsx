821330c6c2784a5a7cf394ce8cb889987328ea3b
amanueltamer <amanueltamer@gmail.com>
2023-04-25 16:02:49 -0500

    adding a like function

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,25 +9,30 @@ import {
 import { Alert, Avatar, Snackbar } from "@mui/material";
 import React, { forwardRef, useEffect, useState } from "react";
 import "../css/Post.css";
-import { db } from "../firebase-config";
+import { auth, db } from "../firebase-config";
 
 import { Tooltip as ReactTooltip } from "react-tooltip";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { selectPost } from "../features/postSlice";
+import { addLike, selectLikeCount, selectPost } from "../features/postSlice";
 
 import firebase from "firebase/compat/app";
 
 const Post = forwardRef(
-  ({ displayName, username, verified, text, image, avatar, postId, commentPost}, ref) => {
+  ({ displayName, username, verified, text, image, avatar, postId, commentPost, likes, likedBy }, ref) => {
     const [like, setLike] = useState(false);
     const [error, setError] = useState(false);
     const [open, setOpen] = useState(false);
 
     const dispatch = useDispatch();
 
+    const likeCount = useSelector(selectLikeCount);
+    // const userId = auth.currentUser.uid;
+
     const navigate = useNavigate();
+    const currentUser = firebase.auth().currentUser;
+    console.log(currentUser)
 
     const openPost = () => {
       dispatch(
@@ -40,15 +45,62 @@ const Post = forwardRef(
           avatar,
           postId,
           commentPost,
+          likes,
+          likedBy,
         })
       );
       navigate(`/thread/${postId}`);
       console.log(displayName);
     };
 
-    const handleLike = () => {
+    console.log(likes)
+    console.log(commentPost)
+
+
+    const handleLike = (postId) => {
       setLike(!like);
+      const user = firebase.auth().currentUser;
+      const postRef = db.collection("posts").doc(postId);
+    
+      postRef.get().then((doc) => {
+        if (doc.exists) {
+          const currentLikes = doc.data().likes || 0;
+          const likedBy = doc.data().likedBy || [];
+    
+          if (likedBy.includes(user.uid)) {
+            // user has already liked the post, so unlike it
+            postRef.update({
+              likes: currentLikes - 1,
+              likedBy: likedBy.filter((userId) => userId !== user.uid),
+            })
+              .then(() => {
+                console.log("Post successfully unliked!");
+              })
+              .catch((error) => {
+                console.error("Error updating post likes: ", error);
+              });
+          } else {
+            // user has not liked the post, so like it
+            postRef.update({
+              likes: currentLikes + 1,
+              likedBy: [...likedBy, user.uid],
+            })
+              .then(() => {
+                console.log("Post successfully liked!");
+              })
+              .catch((error) => {
+                console.error("Error updating post likes: ", error);
+              });
+          }
+        } else {
+          console.log("No such document!");
+        }
+      });
     };
+    
+
+    // console.log(userId)
+    console.log(likeCount)
 
     function deletePost(postId) {
       // Get the currently authenticated user
@@ -144,12 +196,15 @@ const Post = forwardRef(
             <ChatBubbleOutlineOutlined 
             onClick={openPost} className="post__footerIcon" />
             <Repeat className="post__footerIcon" />
-            <Favorite
-              onClick={handleLike}
-              className={
-                like ? "post__footerIcon post__liked" : "post__footerIcon"
-              }
-            />
+            <div className="post__likeButton">
+            <Favorite onClick={() => handleLike(postId)}
+                className={
+                  likedBy?.includes(currentUser?.uid)
+                    ? "post__footerIcon post__liked"
+                    : "post__footerIcon"
+                } />
+                <span className="post__likeCount">{likes}</span>
+            </div>
             <Publish className="post__footerIcon" />
             <Clear
               className="post__footerIcon"
