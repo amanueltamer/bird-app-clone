62bd8684c2f82972f41663ce0f2ede6dd9a8ef64
amanueltamer <amanueltamer@gmail.com>
2023-04-25 16:06:33 -0500

    adding comment and like values to redux

--- src/features/postSlice.js
+++ src/features/postSlice.js
@@ -4,23 +4,48 @@ export const postSlice = createSlice({
   name: "post",
   initialState: {
     selectedPost: null,
-    // prevPostId: null,
     prevPost: null,
+    commentCount: 0,
+    likeCount: 0,
   },
   reducers: {
     selectPost: (state, action) => {
       state.prevPost = state.selectedPost;
       state.selectedPost = action.payload;
-    //   state.prevPostId = prevPost?.postId;
+      state.commentCount = action.payload?.comments?.length || 0;
+      state.likeCount = action.payload?.likes?.length || 0;
     },
+    addComment: (state, action) => {
+      if (state.selectedPost && state.selectedPost.postId === action.payload.postId) {
+        state.selectedPost.comments.push(action.payload.comment);
+        state.commentCount++;
+      }
+    },
+    addLike: (state, action) => {
+        if (state.selectedPost && state.selectedPost.postId === action.payload.postId) {
+          if (!state.selectedPost.likes) {
+            state.selectedPost.likes = []; // initialize likes array if it doesn't exist
+          }
+          state.selectedPost.likes.push(action.payload.like);
+          state.likeCount += 1;
+        }
+      },
+      updatePost: (state, action) => {
+        if (state.selectedPost && state.selectedPost.postId === action.payload.postId) {
+          state.selectedPost = action.payload;
+          state.likeCount = action.payload?.likes?.length || 0;
+          state.commentCount = action.payload?.comments?.length || 0;
+        }
+      },
+      
   },
 });
 
-export const { selectPost } = postSlice.actions;
+export const { selectPost, addComment, addLike, updatePost } = postSlice.actions;
 
 export const selectOpenPost = (state) => state.post.selectedPost;
-
-// export const selectPrevPostId = (state) => state.post.prevPostId;
 export const selectPrevPost = (state) => state.post.prevPost;
+export const selectCommentCount = (state) => state.post.commentCount;
+export const selectLikeCount = (state) => state.post.likeCount;
 
 export default postSlice.reducer;
